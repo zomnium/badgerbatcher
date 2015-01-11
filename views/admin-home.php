d879c865f288526ed628182cd7558a9ae43acfdb
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2015-01-11 01:51:53 +0100

    Content and form added to admin home

--- views/admin-home.php
+++ views/admin-home.php
@@ -1,6 +1,17 @@
 
 <div class="wrap">
-	<h2>This plugin rules and is not working yet :D</h2>
-	<p>Todo: build form</p>
-	<?php var_dump($this->batches); ?>
+	<h2>Badger Batcher</h2>
+	<h3></h3>
+	<p>Always run a backup first. So if something goes wrong you'll still have your content. There is no undo button! Please, do this first.</p>
+	<hr>
+	<h3>Let's run some batch processes on your content!</h3>
+	<form action="">
+		<select name="batch" id="batch">
+		<option disabled selected>Choose your batch process</option>
+		<?php foreach ($this->batches as $key => $value) : ?>
+			<option value="<?php echo $value; ?>"><?php echo $key; ?></option>
+		<?php endforeach; ?>
+		</select>
+		<input type="submit" value="Batch the badger!">
+	</form>
 </div>
