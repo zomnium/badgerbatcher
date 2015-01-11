f0f7f296458d03561786b06eca2f84ecd015783b
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2015-01-11 01:23:27 +0100

    Example for a batch process

---
+++ ExampleBatch.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Example Batch
+ */
+
+class ExampleBatch extends BadgerBatcherAPI implements BadgerBatch
+{
+	/**
+	 * Content
+	 * Get the content that needs processing.
+	 */
+	public function content()
+	{}
+
+	/**
+	 * Execute
+	 * Loop through filtered results.
+	 */
+	public function execute()
+	{}
+
+	/**
+	 * Preprocess
+	 * Runs before the process, useful for any validation.
+	 */
+	public function preprocess()
+	{}
+
+	/**
+	 * Process
+	 * Execute the task(s) that need to be done.
+	 */
+	public function process()
+	{}
+
+	/**
+	 * Postprocess
+	 * Executed after the process, used for updating the content in WordPress.
+	 */
+	public function postprocess()
+	{}
+}
+
+// Add batch process to Badger Batcher
+$badgerbatcher->add('Example: does nothing', 'ExampleBatch');
